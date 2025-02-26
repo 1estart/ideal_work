faa881c706aa5d4030486be53d1ea5f497fa2689
Artemiy <artem.pospelov2018@yandex.ru>
2025-02-26 10:02:23 +0300

    simple bubble sort

--- ideal_work/src/sorting_1.rs
+++ ideal_work/src/sorting_1.rs
@@ -1,5 +1,16 @@
 fn sorting_1(v: Vec<i32>) -> Vec<i32> {
-    Vec::new()
+    let mut v_mut = v;
+    let n = v_mut.len();
+    if n > 1 {
+        for limit in (0..=n - 2).rev() {
+            for i in 0..=limit {
+                if v_mut[i] > v_mut[i + 1] {
+                    v_mut.swap(i, i + 1);
+                }
+            }
+        }
+    }
+    v_mut
 }
 #[cfg(test)]
 mod sorting_1_tests {
@@ -8,5 +19,35 @@ mod sorting_1_tests {
     #[test]
     fn sort_empty() {
         assert_eq!(sorting_1(Vec::new()), Vec::new());
+    }
+
+    #[test]
+    fn sort_with_one_element() {
+        assert_eq!(sorting_1(vec![1]), vec![1]);
+    }
+
+    #[test]
+    fn sort_two_sorted_elements() {
+        assert_eq!(sorting_1(vec![1, 2]), vec![1, 2]);
+    }
+
+    #[test]
+    fn sort_two_unsorted_elements() {
+        assert_eq!(sorting_1(vec![2, 1]), vec![1, 2]);
+    }
+
+    #[test]
+    fn sort_three_sorted_elements() {
+        assert_eq!(sorting_1(vec![1, 2, 3]), vec![1, 2, 3]);
+    }
+
+    #[test]
+    fn sort_three_unsorted_elements_with_two_first() {
+        assert_eq!(sorting_1(vec![2, 1, 3]), vec![1, 2, 3]);
+    }
+
+    #[test]
+    fn sort_three_unsorted_elements_with_two_last_unsorted() {
+        assert_eq!(sorting_1(vec![2, 3, 1]), vec![1, 2, 3]);
     }
 }
